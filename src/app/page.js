58270cefaa360c062cf24fd09d644d8cbf9eef9c
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-07-25 15:47:29 +0200

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Ecommerce Template</h1>
+      <h2>nextjs + prisma + Postgres db</h2>
+    </main>
+  )
+}
