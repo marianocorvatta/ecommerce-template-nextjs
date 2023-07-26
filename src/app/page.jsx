ea26cdd3536255d958350f71b513c3f467399163
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-07-26 14:17:02 +0200

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Ecommerce Template</h1>
-      <h2>nextjs + prisma + Postgres db</h2>
+      <h2>nextjs + prisma + Vercel Postgres db + MercadoPago</h2>
     </main>
   )
 }
