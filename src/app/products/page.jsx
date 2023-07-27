9f3edf1c462476409a1e488cf4f260722ce3f7b0
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-07-27 16:05:37 +0200

    test fetch

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,8 +2,19 @@ import db from '../../../lib/prisma'
 import styles from './Products.module.css'
 import Link from 'next/link'
 
+const baseUrl = process.env.NEXT_PUBLIC_ROOT_DOMAIN
+
 async function getProducts() {
-  const products = await db.product.findMany()
+  // const products = await db.product.findMany()
+  console.log('baseUrl', baseUrl)
+  const res = await fetch(`${baseUrl}/api/product`, { cache: 'no-store' })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const { data: products } = await res.json()
+console.log('products', products)
   return {
     props: products,
   }
