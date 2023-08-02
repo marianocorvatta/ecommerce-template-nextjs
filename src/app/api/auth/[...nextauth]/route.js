8eb84fae1fc4a84497965ac77045d9d00316b22a
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-08-02 17:27:08 +0200

    add todo

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,3 +4,16 @@ import { authOptions } from '../../../../../lib/auth'
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
+
+// TODO
+// import { authOptions } from '@/app/lib/auth';
+// import { getServerSession } from 'next-auth';
+// import { NextResponse } from 'next/server';
+
+// export async function GET(request: Request) {
+//   const session = await getServerSession(authOptions);
+//   console.log(session);
+//   return NextResponse.json({
+//     id: 1,
+//   });
+// }
